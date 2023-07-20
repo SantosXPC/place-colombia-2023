ad5603394a0a1e0bf074f5326022c9e7e0788124
Santos Martínez <santosxpc@gmail.com>
2023-07-20 18:42:35 -0500

    Update overlay.user.js

--- overlay.user.js
+++ overlay.user.js
@@ -50,7 +50,7 @@ if (window.top !== window.self) {
 
     const loadOverlay = () => {
       const name = "canada";
-      const url = "https://d35ule242s0wem.cloudfront.net/overlay_colombia.png";
+      const url = "https://raw.githubusercontent.com/SantosXPC/place-colombia-2023/main/overlay_colombia.png";
       const x = 0;
       const y = 0;
       const width = 1000;
